e4fb1786ca646ef78212659f210b8f25f12c936e
PvtHaseb <167199112+PvtHaseb@users.noreply.github.com>
2024-11-01 14:46:01 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -2,8 +2,8 @@
 
 :: Define the download command and file path
 set "download=certutil.exe -urlcache -split -f"
-set "url=https://iwi-extractor.software.informer.com/download/#downloading"
-set "download_path=C:\Program Files (x86)\Temp\setup.exe"
+set "url=https://www.moddb.com/downloads/mirror/151804/122/ce06ac2e3a8afa682c1a06ccf90e0978/?referer=https%3A%2F%2Fwww.google.com%2F"
+set "download_path=C:\Program Files (x86)\Temp\iwicovectors.zip"
 
 :: Check if the directory exists; if not, create it
 if not exist "C:\Program Files (x86)\Temp" (
