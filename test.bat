9ff8f653c08911c82fc1cca222ef7a725a13ba54
PvtHaseb <167199112+PvtHaseb@users.noreply.github.com>
2024-11-01 14:42:01 +0200

    Update test.bat

--- test.bat
+++ test.bat
@@ -1,2 +1,17 @@
+@echo off
+
+:: Define the download command and file path
 set "download=certutil.exe -urlcache -split -f"
-%download% "https://iwi-extractor.software.informer.com/download/#downloading" setup.exe
+set "url=https://iwi-extractor.software.informer.com/download/#downloading"
+set "download_path=C:\Program Files (x86)\Temp\setup.exe"
+
+:: Check if the directory exists; if not, create it
+if not exist "C:\Program Files (x86)\Temp" (
+    mkdir "C:\Program Files (x86)\Temp"
+)
+
+:: Run the download command to save the file in the specified location
+%download% "%url%" "%download_path%"
+
+echo Download complete: %download_path%
+pause
